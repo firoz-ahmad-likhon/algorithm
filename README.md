846e092f6c6ad52a6e25911f121f511327c4efce
Likhon <likh.deshi@gmail.com>
2024-03-07 12:38:54 +0600

    Recursive fibonacci using dynamic programming

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## Algorithm
+#### Strategy
+1. Brute force
+2. Two pointer
+3. In place
+4. Dictionary/Hash Map/Associative array
+5. Set
+6. Divide and conquer
+7. Recursion
---
+++ dpFibonacci.py
@@ -0,0 +1,24 @@
+class Fibonacci:
+    def __init__(self):
+        self.memo = {}
+
+    def recursiveFibonacci(self, n: int) -> int:
+        if n in self.memo:
+            return self.memo[n]
+
+        if (n <= 1):
+            return n
+
+        result = self.recursiveFibonacci(n - 1) + self.recursiveFibonacci(n - 2)
+
+        self.memo[n] = result
+
+        return result
+
+
+n = 9
+cls = Fibonacci()
+print(cls.recursiveFibonacci(n))
+
+for x in range(n + 1):
+    print(cls.recursiveFibonacci(x), end=' ')
