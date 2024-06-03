32c8cc68a1a52a0d9d098b41ed4d9310ef1c96a5
Likhon <likh.deshi@gmail.com>
2024-06-03 14:22:38 +0600

    Optimized

--- mergeSortedLists.py
+++ mergeSortedLists.py
@@ -1,14 +1,17 @@
 class Sort:
     def merge(self, ls1: list[int], ls2: list[int]) -> list[int]:
         stack = []
+        i = j = 0
         
-        while ls1 and ls2:
-            if(ls1[0] < ls2[0]):
-                stack.append(ls1.pop(0))
+        while i < len(ls1) and j < len(ls2):
+            if(ls1[i] < ls2[j]):
+                stack.append(ls1[i])
+                i += 1
             else:
-                stack.append(ls2.pop(0))
-        stack += ls1
-        stack += ls2
+                stack.append(ls2[j])
+                j += 1
+        stack.extend(ls1[i:])
+        stack.extend(ls2[j:])
         
         return stack
 
